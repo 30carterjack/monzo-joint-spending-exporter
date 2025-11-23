d10877bd26c246ec8a63db596a479af5b2f8e097
Jack Carter <30carterjack@gmail.com>
2025-11-23 11:23:26 +0000

    [JC] feature/excel-export | added a function which extracts the current month from datetime and converts to string e.g., 11 -> November

--- helper.py
+++ helper.py
@@ -1,2 +1,9 @@
+import datetime
+
 def normalise_cost(cost: int):
     return (cost / 100)
+
+def current_month():
+    date = datetime.date.today()
+    month = date.strftime("%B")
+    return month
